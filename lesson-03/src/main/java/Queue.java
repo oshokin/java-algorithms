5927e42c7aeaf85574b99e67df38235bd1ed1cae
o.shokin <shokinoleg@gmail.com>
2022-06-06 22:57:30 +0300

    added lesson 03 homework

--- lesson-03/src/main/java/Queue.java
+++ lesson-03/src/main/java/Queue.java
@@ -1,16 +1,42 @@
 public class Queue {
-    private int maxSize; // размер
-    private int[] queue; // место хранения
-    private int head;    // отсюда уходят
-    private int tail;    // сюда приходят
-    private int items;   // текущее количество
+    protected static final byte defaultLoadFactor = 2;
 
-    public Queue(int s) {
-        maxSize = s;
+    protected int maxSize;        // размер
+    protected byte loadFactor;    // коэффициент увеличения размера
+    protected int[] queue;        // место хранения
+    protected int head;           // отсюда уходят
+    protected int tail;           // сюда приходят
+    protected int items;          // текущее количество
+
+    public Queue(int size) {
+        this(size, defaultLoadFactor);
+    }
+
+    public Queue(int size, byte lf) {
+        maxSize = size;
+        loadFactor = lf;
         queue = new int[maxSize];
         head = 0;
         tail = -1;
         items = 0;
+    }
+
+    public Queue(int[] original) {
+        this(original, defaultLoadFactor);
+    }
+
+    public Queue(int[] original, byte lf) {
+        loadFactor = lf;
+        if (loadFactor <= 0) loadFactor = defaultLoadFactor;
+        maxSize = original.length;
+        var copy = new int[maxSize];
+        System.arraycopy(original, 0, copy, 0, maxSize);
+
+        loadFactor = lf;
+        queue = copy;
+        head = 0;
+        tail = maxSize - 1;
+        items = maxSize;
     }
 
     public boolean isEmpty() {
@@ -25,34 +51,59 @@ public class Queue {
         return items;
     }
 
-    public void insert(int i) {
-        if (isFull()) {
-            maxSize *= 2;
-            int[] tmpArr = new int[maxSize];
-            if (tail >= head) {
-                System.arraycopy(queue, 0, tmpArr, 0, queue.length);
-            } else {
-                System.arraycopy(queue, 0, tmpArr, 0, tail + 1);
-                System.arraycopy(queue, head, tmpArr,
-                        maxSize - (queue.length - head), queue.length - head);
-                head = maxSize - head - 1;
-            }
-        }
-        if (tail == maxSize - 1) {
-            tail = -1;
-        }
+    public void pushLast(int i) {
+        grow();
+        if (tail == maxSize - 1) tail = -1;
         queue[++tail] = i;
         ++items;
     }
 
-    public int remove() {
+    public int popFirst() {
+        if (items == 0) throw new RuntimeException("Something got broken definitely!");
         int temp = queue[head++];
-        head %= maxSize;
-        items--;
+        if (head >= maxSize) head = 0;
+        if (--items == 0) {
+            head = 0;
+            tail = -1;
+        }
+
         return temp;
     }
 
-    public int peek() {
+    public int peekFirst() {
+        if (items == 0) throw new RuntimeException("Oye, que estas haciendo mi helmanico?");
         return queue[head];
+    }
+
+    public String toString() {
+        if (items == 0) return "[]";
+        StringBuilder b = new StringBuilder();
+        b.append('[');
+        for (int i = head; ; i++) {
+            if (i == maxSize) i = 0;
+            b.append(queue[i]);
+            if (i == tail) {
+                b.append(']');
+                break;
+            }
+            b.append(", ");
+        }
+
+        return b.toString();
+    }
+
+    protected void grow() {
+        if (!isFull()) return;
+        maxSize *= loadFactor;
+        int[] tmpArr = new int[maxSize];
+        if (tail >= head) {
+            System.arraycopy(queue, 0, tmpArr, 0, queue.length);
+        } else {
+            System.arraycopy(queue, 0, tmpArr, 0, tail + 1);
+            var newHead = maxSize - (queue.length - head);
+            System.arraycopy(queue, head, tmpArr, newHead, queue.length - head);
+            head = newHead;
+        }
+        queue = tmpArr;
     }
 }
